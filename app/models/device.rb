57e17374b213a65fed4287ed705c782c2577cc4e
Richard Davis <rdavis.bacs@gmail.com>
2015-01-15 19:50:28 -0500

    no hashrocket

--- app/models/device.rb
+++ app/models/device.rb
@@ -9,23 +9,23 @@ class Device < ActiveRecord::Base
 
   state_machine initial: :en_route do
     event :receive do
-      transition :en_route => :received
+      transition en_route: :received
     end
 
     event :inspect do
-      transition :received => :inspected
+      transition received: :inspected
     end
 
     event :repair do
-      transition :inspected => :repaired
+      transition inspected: :repaired
     end
 
     event :package do
-      transition :repaired => :packaged
+      transition repaired: :packaged
     end
 
     event :shipped do
-      transition :packaged => :shipped
+      transition packaged: :shipped
     end
   end
 end
